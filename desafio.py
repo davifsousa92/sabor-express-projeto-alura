f3299732857aa9860f6ae3b55d317405850af1d3
Davi Ferreira <davi.fsousa92@hotmail.com>
2024-05-19 16:06:48 -0300

    projeto inicial

---
+++ desafio.py
@@ -0,0 +1,18 @@
+print("""
+      Escola de Programação da Alura
+      """)
+
+nome = input('Digite o seu nome: \n')
+idade = input('Digite a sua idade: \n')
+print(f'Meu nome é {nome} e tenho {idade} anos')
+
+print("""
+A
+L
+U
+R
+A
+""")
+pi = 3.14159
+pi_arredondado = round(pi, 2)
+print(f'O valor arrendondado de pi é: {pi_arredondado}')
